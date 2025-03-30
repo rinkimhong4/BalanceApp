eeb3474eb9482aa9eeea6484cf44171b11fcac57
rin kimhong <you@example.com>
2025-03-31 02:50:00 +0700

    create BottomNavigationBar 3/31/2025

---
+++ lib/pages/balance.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BalanceScreen extends StatelessWidget {
+  const BalanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Balance')));
+  }
+}
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:balance/pages/balance.dart';
+import 'package:balance/pages/task.dart';
+import 'package:balance/pages/timer.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedPage = 0;
+
+  final _listPages = [BalanceScreen(), TaskScreen(), TimerScreen()];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('hi'), centerTitle: true),
+      body: _listPages[_selectedPage],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              _selectedPage == 0
+                  ? Icons.account_balance
+                  : Icons.account_balance_outlined,
+            ),
+            label: 'Balance',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(_selectedPage == 1 ? Icons.task : Icons.task_outlined),
+            label: 'Task',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(_selectedPage == 2 ? Icons.timer : Icons.timer_outlined),
+            label: 'Timer',
+          ),
+        ],
+        currentIndex: _selectedPage,
+        onTap: (index) {
+          setState(() {
+            _selectedPage = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Color(0xFFFF7622),
+        unselectedItemColor: Colors.black,
+        backgroundColor: const Color(0xFFF2F8FC),
+      ),
+    );
   }
 }
