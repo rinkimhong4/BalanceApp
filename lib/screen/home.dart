04ca55d97d704e11d4d230fce19e92c38df4181e
rin kimhong <you@example.com>
2025-03-31 19:10:43 +0700

    Update btnnavigation2

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -19,60 +19,50 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(title: const Text('hi'), centerTitle: true),
       body: _listPages[_selectedPage],
       backgroundColor: Colors.white,
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              _selectedPage == 0 ? 'assets/bank-2.svg' : 'assets/bank-1.svg',
-              width: _iconsSize,
-              height: _iconsSize,
-            ),
-            // icon: Icon(
-            //   _selectedPage == 0
-            //       ? Icons.account_balance
-            //       : Icons.account_balance_outlined,
-            // ),
-            label: 'Balance',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              _selectedPage == 1 ? 'assets/task-2.svg' : 'assets/task-1.svg',
-              width: _iconsSize,
-              height: _iconsSize,
-            ),
-            //icon: Icon(_selectedPage == 1 ? Icons.task : Icons.task_outlined),
-            label: 'Task',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              _selectedPage == 2 ? 'assets/timer-2.svg' : 'assets/timer-1.svg',
-              width: _iconsSize,
-              height: _iconsSize,
-            ),
-            // icon: Icon(_selectedPage == 2 ? Icons.timer : Icons.timer_outlined),
-            label: 'Timer',
-          ),
-        ],
-        currentIndex: _selectedPage,
-        onTap: (index) {
-          setState(() {
-            _selectedPage = index;
-          });
-        },
-
-        type: BottomNavigationBarType.fixed,
-        // //when unselected dont show label
-        // showUnselectedLabels: false,
-        // //show when selected label
-        // showSelectedLabels: true,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black,
-        selectedLabelStyle: TextStyle(fontSize: 13.0, fontFamily: 'Inter'),
-        unselectedLabelStyle: TextStyle(fontSize: 12.0, fontFamily: 'Inter'),
-      ),
+      bottomNavigationBar: _buildBottomNavigationBar,
     );
   }
+
+  get _buildBottomNavigationBar => BottomNavigationBar(
+    items: [
+      BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          _selectedPage == 0 ? 'assets/bank-2.svg' : 'assets/bank-1.svg',
+          width: _iconsSize,
+          height: _iconsSize,
+        ),
+
+        label: 'Balance',
+      ),
+      BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          _selectedPage == 1 ? 'assets/task-2.svg' : 'assets/task-1.svg',
+          width: _iconsSize,
+          height: _iconsSize,
+        ),
+        label: 'Task',
+      ),
+      BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          _selectedPage == 2 ? 'assets/timer-2.svg' : 'assets/timer-1.svg',
+          width: _iconsSize,
+          height: _iconsSize,
+        ),
+        label: 'Timer',
+      ),
+    ],
+    currentIndex: _selectedPage,
+    onTap: (index) {
+      setState(() {
+        _selectedPage = index;
+      });
+    },
+    type: BottomNavigationBarType.fixed,
+    selectedItemColor: Colors.black,
+    unselectedItemColor: Colors.black,
+    selectedLabelStyle: TextStyle(fontSize: 13.0, fontFamily: 'Inter'),
+    unselectedLabelStyle: TextStyle(fontSize: 12.0, fontFamily: 'Inter'),
+  );
 }
