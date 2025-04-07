bc2b704fd4bcbeb14dc57a69ff4ed08549ddb6c0
rin kimhong <you@example.com>
2025-04-07 11:48:06 +0700

    first commit balance body

--- lib/pages/balance.dart
+++ lib/pages/balance.dart
@@ -10,15 +10,20 @@ class BalanceScreen extends StatefulWidget {
 }
 
 class _BalanceScreenState extends State<BalanceScreen> {
+  final List _titleBudget = ['Income', 'Expense'];
+  final List _iconBudget = [
+    Icons.arrow_circle_up_sharp,
+    Icons.arrow_circle_down_sharp,
+  ];
+  final List _dBudget = ['\$50,000.00', '\$1,000.00'];
+  final List _colorBudget = [const Color(0xFF05a56a), Colors.red];
+  final Color primaryColor = const Color(0xFF3629B7);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _appBar,
-      body: const Center(child: Text('Balance Screen Content')),
-    );
+    return Scaffold(appBar: _appBar, body: _buildBody);
   }
 
-  Color primaryColor = Color(0xFF3629B7);
   get _appBar => AppBar(
     title: Text(
       'Hi, Kimhong!',
@@ -38,5 +43,69 @@ class _BalanceScreenState extends State<BalanceScreen> {
     ],
   );
 
-  // get _tabBarApp => TabBar()
+  get _buildBody => Container(
+    padding: const EdgeInsets.only(top: 16),
+    margin: const EdgeInsets.all(16),
+    height: 130,
+    width: double.infinity,
+    decoration: BoxDecoration(
+      //color: Colors.white,
+      borderRadius: BorderRadius.circular(16),
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: List.generate(
+        _titleBudget.length,
+        (index) => Container(
+          padding: const EdgeInsets.all(16),
+          width: 140,
+          decoration: BoxDecoration(
+            color: _colorBudget[index].withOpacity(0.1),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(_iconBudget[index], color: _colorBudget[index]),
+                  const SizedBox(width: 5),
+                  Text(
+                    _titleBudget[index],
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: _colorBudget[index],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    _dBudget[index],
+                    style: GoogleFonts.inter(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: _colorBudget[index],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+
+  get _tabBarBottom => TabBar(
+    indicatorColor: primaryColor,
+    labelColor: primaryColor,
+    unselectedLabelColor: Colors.black,
+    tabs: const [Tab(text: 'All'), Tab(text: 'Income'), Tab(text: 'Expense')],
+  );
 }
